9419889786436f506b8ff999fd5d9159825221bc
unknown <nataliamartineli@gmail.com>
2022-06-30 20:12:07 -0300

    Refactor evento checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -34,13 +34,14 @@ function App() {
 
 		// setEventos([...eventos]);
 	};
-	const alterarStatusEvento = (id: number) => {
-		// const evento = eventos.find((evento) => evento.id === id);
-		// 	if (evento) {
-		// 		evento.completo = !evento.completo;
-		// 	}
-		// 	setEventos([...eventos]);
-	};
+
+	// const alterarStatusEvento = (id: number) => {
+	// 	// const evento = eventos.find((evento) => evento.id === id);
+	// 	// 	if (evento) {
+	// 	// 		evento.completo = !evento.completo;
+	// 	// 	}
+	// 	// 	setEventos([...eventos]);
+	// };
 
 	// const deletarEvento = (id: number) => {
 	// 	// setEventos([...eventos.filter((evento) => evento.id !== id)]);
@@ -63,7 +64,7 @@ function App() {
 					</Card>
 					<hr />
 					<Card>
-						<ListaDeEventos aoFiltroAplicado={aplicarFiltro} aoAlterarStatus={alterarStatusEvento} />
+						<ListaDeEventos aoFiltroAplicado={aplicarFiltro} />
 					</Card>
 				</div>
 				<div className={style.Coluna}>{<Calendario />}</div>
